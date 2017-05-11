6dd383582d12952ebdef5cde26183e4c19f7d828
Hiep Nguyen <ngochiep9523@gmail.com>
2017-05-11 22:05:11 +0700

    Fix minor bug wrong variable name

--- server_config.js
+++ server_config.js
@@ -2,10 +2,10 @@ var sql = require('mssql');
 //2.
 function ServerConfig() {
 	this.config = {
-	    server: '89cb38b8-d521-496f-aaba-a72b003f22c3.sqlserver.sequelizer.com',
-	    database: 'db89cb38b8d521496faabaa72b003f22c3',
-	    user: 'tdgmcttqjhveiwyd',
-	    password: 'x6FyKB84fFVptdggFftH36rVjzxj27yD3um4bxLrhuyF7EttjTLoHynAMSpkdXch',
+	    server: '79b3b01c-441b-4737-bd3a-a76e004f1ef6.sqlserver.sequelizer.com',
+	    database: 'db79b3b01c441b4737bd3aa76e004f1ef6',
+	    user: 'sncwvkluwaeksmom',
+	    password: 'SgCygGCadzZjv6vgUrhL7KrFMAAch7TV6hV7bEhmdeXyFrA5NeMyv5FhX5TNfz2f',
 	    port: 1433
 	};
 	this.amqpURL = 'amqp://imtqjgzz:LQWyhmVxKBMgV6ROObew36G07DUs6ZYZ@white-mynah-bird.rmq.cloudamqp.com/imtqjgzz';
